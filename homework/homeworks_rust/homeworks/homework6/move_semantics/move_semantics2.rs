083b4cecc90dd0d98b08b971220368495397dc32
Kitan Garcia <kitangar@gmail.com>
2023-09-06 19:46:46 -0700

    hw6 - complete move semantics

--- homework/homeworks_rust/homeworks/homework6/move_semantics/move_semantics2.rs
+++ homework/homeworks_rust/homeworks/homework6/move_semantics/move_semantics2.rs
@@ -2,12 +2,38 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
+// 1. Borrow value of vec0 using &mut 
+fn main() {
+    let mut vec0 = Vec::new();
+
+    let mut vec1 = fill_vec(&mut vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec1.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+}
+
+fn fill_vec(vec: &mut Vec<i32>) -> Vec<i32> {
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec.to_vec()
+}
+
+/*
+   Other working solutions:
+2. Copy vec 0 into another variable so it doesn't lose ownership
 
 fn main() {
-    let vec0 = Vec::new();
+    let mut vec0 = Vec::new();
+    let vec0Copy = vec0.clone();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0Copy);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -26,3 +52,24 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
     vec
 }
+
+2. Get rid of vec1 and directly modify vec0
+
+fn main() {
+    let mut vec0 = Vec::new();
+
+    vec0 = fill_vec(vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+}
+
+fn fill_vec(mut vec: Vec<i32>) -> Vec<i32> {
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+ */
