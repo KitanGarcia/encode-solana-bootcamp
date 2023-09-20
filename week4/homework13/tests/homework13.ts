cb32c39aa8cf878fd215f4e4d1bc89d425f5a4ac
Kitan Garcia <kitangar@gmail.com>
2023-09-20 12:02:06 -0700

    get homework 13 to workable state on devnet and localnet

--- week4/homework13/tests/homework13.ts
+++ week4/homework13/tests/homework13.ts
@@ -3,6 +3,11 @@ import { Program } from "@coral-xyz/anchor";
 import { Homework13 } from "../target/types/homework13";
 
 import { Connection, Keypair, PublicKey } from "@solana/web3.js";
+import { IDL } from "../target/types/hw13";
+
+export function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 describe("homework13", () => {
   // Configure the client to use the local cluster.
@@ -11,31 +16,45 @@ describe("homework13", () => {
   const provider = anchor.AnchorProvider.local();
   */
   //anchor.setProvider(anchor.AnchorProvider.local());
-  const provider = anchor.AnchorProvider.env();
+  const PROGRAM_ID = "GovAH137uoY4ifyfTNQjesKNJKo55tMF435dBfwZ22DG";
+  const oldProvider = anchor.AnchorProvider.env();
 
-  const program = anchor.workspace.Homework13 as Program<Homework13>;
+  const devnetURL = "https://api.devnet.solana.com";
+  const localnetURL = "http://localhost:8899";
+  const connection = new anchor.web3.Connection(localnetURL);
+  const provider = new anchor.AnchorProvider(
+    connection,
+    oldProvider.wallet,
+    oldProvider.opts
+  );
+  const program = new Program(IDL, PROGRAM_ID, provider);
 
   it("Is initialized!", async () => {
     const balanceStructKeypair = Keypair.generate();
-    console.log(balanceStructKeypair);
-    console.log(provider.wallet.publicKey);
 
-    const tx = await program.methods
-      .initialize()
-      .accounts({
-        balanceStruct: balanceStructKeypair.publicKey,
-        authority: provider.publicKey,
-        systemProgram: anchor.web3.SystemProgram.programId,
-      })
-      .signers([balanceStructKeypair])
-      .rpc();
+    try {
+      const tx = await program.methods
+        .initialize()
+        .accounts({
+          balanceStruct: balanceStructKeypair.publicKey,
+          authority: provider.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        })
+        .signers([balanceStructKeypair])
+        .rpc();
 
-    const balancesStructAccount = await program.account.balanceStruct.fetch(
-      balanceStructKeypair.publicKey
-    );
+      console.log("Your transaction signature:", tx);
 
-    console.log("===================================================");
-    console.log("Balance:", balancesStructAccount.balance.toString());
-    console.log("Your transaction signature:", tx);
+      await sleep(20000); // not work then take higer value
+
+      const balancesStructAccount = await program.account.balanceStruct.fetch(
+        balanceStructKeypair.publicKey
+      );
+
+      console.log("===================================================");
+      console.log("Balance:", balancesStructAccount.balance.toString());
+    } catch (error) {
+      console.log("ERROR:", error);
+    }
   });
 });
